8b5629a4bfa50c727c9a293a3236c980dc16f8c3
nohnt9 <nohanbt9@gmail.com>
2023-07-28 01:56:28 +0530

    15 commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const distPath=path.join(__dirname,"dist");
 //app.use(express.static(distPath));
 app.get("/",(req,res)=>{
     console.log("request came");
-res.sendFile(path.join(distPath,"index.html"));
+res.sendFile(path.join(distPath,"index"));
 });
 
 
